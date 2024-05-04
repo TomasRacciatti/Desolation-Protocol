6ba899a17dfdcc8484be5df4ea647e608fc08d25
pisto98 <lautaro.pistolessi@davinci.edu.ar>
2024-05-04 17:25:24 -0300

    armado de torre y script

--- DesolationProtocol/Assets/Entities/General/Turret/ScTurret.cs
+++ DesolationProtocol/Assets/Entities/General/Turret/ScTurret.cs
@@ -4,9 +4,31 @@ using UnityEngine;
 
 public class ScTurret : MonoBehaviour
 {
-  
+    public GameObject Bullet;
+    public Transform Player;
+    public float Range;
+    public float RangeOfSight;
+    public Transform Rotator;
+    public Transform ShootPoint;
 
+    private void Start()
+    {
+        //Player = FindObjectOfType<Player>().transform;           
+    }
 
+    private void Update()
+    {
+        float Distance = Vector3.Distance(transform.position, Player.position);
+        print(Distance);
+    }
 
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(Rotator.position, Range);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(Rotator.position, RangeOfSight);
+    }
 
 }
