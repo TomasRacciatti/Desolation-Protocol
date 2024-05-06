d955a1c1b4b154f29b1a5e40cce26ef01d94b55e
pisto98 <lautaro.pistolessi@davinci.edu.ar>
2024-05-06 18:25:51 -0300

    oleada + unificacion de script

--- DesolationProtocol/Assets/HUD/Scripts/ScGM.cs
+++ DesolationProtocol/Assets/HUD/Scripts/ScGM.cs
@@ -4,28 +4,38 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class Ammo : MonoBehaviour
+public class ScGM : MonoBehaviour
 {
     [SerializeField] private int _Ammo;
     [SerializeField] private int _AmmoMax;
     [SerializeField] private bool IsFiring;
     public Text AmmoCounter;
+    
+    public Text WaveCounter;
+    [SerializeField] private int _Wave;
 
 
     void Start()
     {
         _Ammo = _AmmoMax;
+        _Wave = 1;
     }
 
 
     void Update()
     {
         AmmoCounter.text = _Ammo.ToString();
+        WaveCounter.text = "Oleada " + _Wave.ToString();
         if (Input.GetMouseButtonDown(0) && !IsFiring && _Ammo > 0)
         {
             IsFiring = true;
             _Ammo--;
             IsFiring=false;
         }
+    }
+
+    public void WaveIncrease()
+    {
+        _Wave++;
     }
 }
