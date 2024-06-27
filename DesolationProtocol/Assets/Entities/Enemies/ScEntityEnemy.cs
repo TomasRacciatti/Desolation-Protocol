1cc0a83856bbeafc2585ecbd9158003489ea4352
TomasRacciatti <tomasracciatti@gmail.com>
2024-06-27 12:23:23 -0300

    Script del entity modificado para subir escaleras

--- DesolationProtocol/Assets/Entities/Enemies/ScEntityEnemy.cs
+++ DesolationProtocol/Assets/Entities/Enemies/ScEntityEnemy.cs
@@ -66,4 +66,45 @@ public class ScEntityEnemy : ScEntity
             _agent.SetDestination(transform.position);
         }
     }
+
+    protected override void StepClimb()
+    {
+        RaycastHit hitLower;
+        if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(Vector3.forward), out hitLower, 0.1f))
+        {
+            RaycastHit hitUpper;
+            if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(Vector3.forward), out hitUpper, 0.2f))
+            {
+                AdjustAgentPosition(new Vector3(0f, stepSmooth, 0f));
+            }
+        }
+
+        RaycastHit hitLower45;
+        if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(1.5f, 0, 1), out hitLower45, 0.1f))
+        {
+            RaycastHit hitUpper45;
+            if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(1.5f, 0, 1), out hitUpper45, 0.2f))
+            {
+                AdjustAgentPosition(new Vector3(0f, stepSmooth, 0f));
+            }
+        }
+
+        RaycastHit hitLowerMinus45;
+        if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(-1.5f, 0, 1), out hitLowerMinus45, 0.1f))
+        {
+            RaycastHit hitUpperMinus45;
+            if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(-1.5f, 0, 1), out hitUpperMinus45, 0.2f))
+            {
+                AdjustAgentPosition(new Vector3(0f, stepSmooth, 0f));
+            }
+        }
+    }
+
+    private void AdjustAgentPosition(Vector3 adjustment)
+    {
+        _agent.updatePosition = false;
+        _rigidbody.position += adjustment;
+        _agent.nextPosition = _rigidbody.position;
+        _agent.updatePosition = true;
+    }
 }
